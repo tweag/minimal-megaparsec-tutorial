77f6d930abb0fb74e3ca9b61fceb0ffee4acca46
Clément Hurlin <clement.hurlin@moduscreate.com>
2025-04-14 16:22:46 +0200

    Add renderer and example map

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,34 @@
 module Main where
 
--- import qualified Lib (someFunc)
+import Data.Text.IO qualified as T
+import System.Environment (getArgs)
+import System.Exit (die)
+import System.IO (stdin)
+
+import Text.Megaparsec qualified as Parsec
+
+import Data.Maybe (fromMaybe)
+import Lib qualified
+import Render qualified
 
 main :: IO ()
 main = do
-  putStrLn "Hello, Haskell!"
+  args <- getArgs
+  (content, source) <- case args of
+    ["--input-map", filepath] -> do
+      result <- T.readFile filepath
+      return (result, Just filepath)
+    [] -> do
+      result <- T.hGetContents stdin
+      return (result, Nothing)
+    _ -> die "Usage: ./min-mega-exe [--input-map <FILE>]"
+  case runParser source Lib.parseElements content of
+    Left err -> die $ "Error parsing map: " ++ err
+    Right elements -> do
+      Render.render elements
+
+runParser :: Maybe FilePath -> Lib.Parser a -> Lib.Input -> Either Lib.Error a
+runParser filepath parser url =
+  case Parsec.runParser parser (fromMaybe "" filepath) url of
+    Left err -> Left $ Parsec.errorBundlePretty err
+    Right x -> Right x
